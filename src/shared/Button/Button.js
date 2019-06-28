161c0ef739cf068b008b98c1e4794b6cdd5127a8
r4lfl4b <dev@rgalan.es>
2019-06-28 17:38:22 +0100

    feat: add Button component

---
+++ src/shared/Button/Button.js
@@ -0,0 +1,40 @@
+import './Button.scss';
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+
+const propTypes = {
+  /**
+   * Specifies button click action
+   */
+  onClick: PropTypes.func,
+
+  /**
+   * Set button text
+   */
+  children: PropTypes.string,
+
+  /**
+   * Set custom CSS classnames
+   */
+  className: PropTypes.string
+};
+
+const Button = props => {
+  const { className, children, onClick = () => {} } = props;
+  const handleClick = () => {
+    onClick();
+  };
+
+  const buttonClasses = cx('fi-button');
+
+  return (
+    <button className={`${buttonClasses} ${className}`} onClick={handleClick}>
+      {children}
+    </button>
+  );
+};
+
+Button.propTypes = propTypes;
+
+export default Button;
